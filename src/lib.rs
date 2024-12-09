44bea620687f59f71c6362c086e1ea00828e27fe
Justin Kur <Justink97@hotmail.com>
2024-12-09 18:30:21 -0500

    Simple benchmarking routine

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,14 @@ pub mod p6;
 pub mod p7;
 pub mod p8;
 pub mod p9;
+
+pub fn simple_bench<O>(input: &str, function: fn(&str) -> O) {
+    benchmarking::warm_up();
+
+    let bench_result = benchmarking::bench_function(|measurer| {
+        measurer.measure(|| function(input));
+    })
+    .unwrap();
+    assert!(bench_result.times() > 0);
+    eprintln!("Average duration: {:?}", bench_result.elapsed());
+}
